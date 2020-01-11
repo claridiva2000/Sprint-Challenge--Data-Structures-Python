6c234c67804d14217e1974d8e0a77c016728ca06
claridiva2000 <claridiva2000@gmail.com>
2020-01-11 12:01:24 -0600

    initial commit. names done

--- names/names.py
+++ names/names.py
@@ -1,5 +1,65 @@
 import time
 
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        #if the new value is smaller than the existing node
+        if value < self.value:
+            #if not left  means "if left is null"
+            if not self.left:
+                #create a new node
+                self.left = BinarySearchTree(value)
+            else:
+                #once the recursion is broken, we insert the value
+                self.left.insert(value)
+        else:
+            #if the right child is
+            if not self.right:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        if self.value == target:
+            return True
+        if target < self.value:
+            if not self.left:
+                return False
+            else:
+                return self.left.contains(target)
+        else:
+            if not self.right:
+                return False
+            else:
+                return self.right.contains(target)
+        
+    # Return the maximum value found in the tree
+    def get_max(self):
+        if not self:
+            return None
+        if not self.right:
+            return self.value
+        else:
+            return self.right.get_max()
+
+    # Call the function `cb` on the value of each node
+    # You may use a recursive or iterative approach
+    def for_each(self, cb):
+        cb(self.value)
+
+        if self.left:
+            self.left.for_each(cb)
+        if self.right:
+            self.right.for_each(cb)
+
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -10,11 +70,22 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+names_list = BinarySearchTree(names_1[0])
+
+for name in names_1[1:]:
+    names_list.insert(name)
+
+duplicates = [] 
+
+for name2 in names_2:
+    if names_list.contains(name2):
+        duplicates.append(name2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
